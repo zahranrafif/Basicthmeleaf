9a704f8c418a2951d88a927d4d34af95cc199286
Zahran Rafif <zahranlumia@gmail.com>
2022-10-31 18:58:18 +0700

    fix BioDataController.java

--- src/main/java/ws/b/takehome01/BioDataController.java
+++ src/main/java/ws/b/takehome01/BioDataController.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
  *
  * @author Zahran Rafif Pc
  */
+@Controller
 public class BioDataController {
     //Home
     @RequestMapping("/")
